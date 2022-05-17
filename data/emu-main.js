0b5f4425c3f24871dad02ce836188cf4a139e57d
Allan Niles <74841470+allancoding@users.noreply.github.com>
2022-05-16 18:01:37 -0600

    Update

--- data/emu-main.js
+++ data/emu-main.js
@@ -6530,23 +6530,3 @@ window.EJS_main = function(_0xa88a13, _0x17edbf, _0x2c1832) {
     _0x3dbc76.defaults = {};
     _0x17edbf.default = _0x3dbc76;
 }
-
-$(document).ready(function(){
-    $(".ejs--73f9b4e94a7a1fe74e11107d5ab2ef").attr("id","start");
-    $(".ejs--cbcfe0a1421cadac9a04c81d6431d6").attr("id","loding");
-    $(".ejs--71527b6509aa48afce3ce1a11c02f0").attr("id","naming");
-    $(".ejs--057800d021995e1347ec07cb748672").addClass("backg");
-    $("#loding").attr("hidden","");
-    $("#loding").hide();
-    $("#start").on('touchstart click', function() {
-    $(".ejs--057800d021995e1347ec07cb748672").removeClass("backg");
-    $("#loding").show();
-    $("#loding").removeAttr("hidden");
-    $("div").remove(".ejs--f3a1903d935f6cf720d4a0498db62a");
-    setTimeout(
-  function() 
-  {
-    $("div").remove(".ejs--cbcfe0a1421cadac9a04c81d6431d6");
-  }, 4500);
-});
-});
