bc046470d7c86d2241b374ed9b8fe0122a7b637d
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2023-08-01 13:50:21 -0500

    Update RetroArch

--- data/emulator.js
+++ data/emulator.js
@@ -1,5 +1,5 @@
 class EmulatorJS {
-    version = 3; //Increase by 1 when cores are updated
+    version = 4; //Increase by 1 when cores are updated
     getCore(generic) {
         const core = this.config.system;
         /*todo:
