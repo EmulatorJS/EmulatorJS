7c61b169456d78db75ae5126880939b21412c4bc
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2024-03-07 13:59:29 -0600

    Enable vsync by default

--- data/emulator.js
+++ data/emulator.js
@@ -4290,7 +4290,7 @@ class EmulatorJS {
         addToMenu(this.localization("VSync"), "vsync", {
             'enabled': this.localization("Enabled"),
             'disabled': this.localization("Disabled")
-        }, "disabled");
+        }, "enabled");
         
         addToMenu(this.localization('Fast Forward Ratio'), 'ff-ratio', [
             "1.5", "2.0", "2.5", "3.0", "3.5", "4.0", "4.5", "5.0", "5.5", "6.0", "6.5", "7.0", "7.5", "8.0", "8.5", "9.0", "9.5", "10.0", "unlimited"
