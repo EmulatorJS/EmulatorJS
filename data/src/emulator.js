cc34eb7b391bc5a683cd0c32cb2ac120de7186e7
pjft <pjft@users.noreply.github.com>
2025-03-07 14:28:43 +0000

    Adding EJS_dontExtractBIOS option

--- data/src/emulator.js
+++ data/src/emulator.js
@@ -680,7 +680,7 @@ class EmulatorJS {
                 return resolve(assetUrl);
             }
             const gotData = async (input) => {
-                if (['arcade', 'mame'].includes(this.getCore(true))) {
+                if (window.EJS_dontExtractBIOS === true) {
                     this.gameManager.FS.writeFile(assetUrl, new Uint8Array(input));
                     return resolve(assetUrl);
                 }
