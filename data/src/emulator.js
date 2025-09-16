3ebc70f72e245d613727b007153c89db28c24f68
Michael Green <84688932+michael-j-green@users.noreply.github.com>
2025-09-16 15:22:51 +1000

    Refactor takeScreenshot method to return a promise with screenshot data and format; update input element in HTML for better accessibility.

--- data/src/emulator.js
+++ data/src/emulator.js
@@ -7040,16 +7040,14 @@ class EmulatorJS {
         }
     }
 
-    async takeScreenshot(source, format, upscale) {
-        let blob = await new Promise((resolve) => {
-            this.screenshot((blob, format) => {
-                resolve(blob, format);
+    takeScreenshot(source, format, upscale) {
+        return new Promise((resolve) => {
+            this.screenshot(async (blob, returnFormat) => {
+                const arrayBuffer = await blob.arrayBuffer();
+                const uint8 = new Uint8Array(arrayBuffer);
+                resolve({ screenshot: uint8, format: returnFormat });
             }, source, format, upscale);
         });
-
-        const arrayBuffer = await blob.arrayBuffer();
-        const uint8 = new Uint8Array(arrayBuffer);
-        return { screenshot: uint8, format: format };
     }
 
     collectScreenRecordingMediaTracks(canvasEl, fps) {
