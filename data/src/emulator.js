353b99a449118f80be67c7820830e296509d6cf0
Ethan O'Brien <ethan.a.obrien@gmail.com>
2026-03-05 15:30:32 -0600

    Move consts to another file

--- data/src/emulator.js
+++ data/src/emulator.js
@@ -6,63 +6,24 @@ import { EJS_SHADERS } from "./shaders.js";
 import { EJS_STORAGE, EJS_DUMMYSTORAGE } from "./storage.js";
 import { EJS_UTILS } from "./utils.js";
 import { EJS_SETUP } from "./setup.js";
+import * as CONSTS from "./consts.js";
 
 import "./vendor/nipplejs.js";
 import "./vendor/socket.io.min.js";
 
 class EmulatorJS {
     getCores() {
-        let rv = {
-            "atari5200": ["a5200"],
-            "vb": ["beetle_vb"],
-            "nds": ["melonds", "desmume", "desmume2015"],
-            "arcade": ["fbneo", "fbalpha2012_cps1", "fbalpha2012_cps2", "same_cdi"],
-            "nes": ["fceumm", "nestopia"],
-            "gb": ["gambatte"],
-            "coleco": ["gearcoleco"],
-            "segaMS": ["smsplus", "genesis_plus_gx", "genesis_plus_gx_wide", "picodrive"],
-            "segaMD": ["genesis_plus_gx", "genesis_plus_gx_wide", "picodrive"],
-            "segaGG": ["genesis_plus_gx", "genesis_plus_gx_wide"],
-            "segaCD": ["genesis_plus_gx", "genesis_plus_gx_wide", "picodrive"],
-            "sega32x": ["picodrive"],
-            "sega": ["genesis_plus_gx", "genesis_plus_gx_wide", "picodrive"],
-            "lynx": ["handy"],
-            "mame": ["mame2003_plus", "mame2003"],
-            "ngp": ["mednafen_ngp"],
-            "pce": ["mednafen_pce"],
-            "pcfx": ["mednafen_pcfx"],
-            "psx": ["pcsx_rearmed", "mednafen_psx_hw"],
-            "ws": ["mednafen_wswan"],
-            "gba": ["mgba"],
-            "n64": ["mupen64plus_next", "parallel_n64"],
-            "3do": ["opera"],
-            "psp": ["ppsspp"],
-            "atari7800": ["prosystem"],
-            "snes": ["snes9x", "bsnes"],
-            "atari2600": ["stella2014"],
-            "jaguar": ["virtualjaguar"],
-            "segaSaturn": ["yabause"],
-            "amiga": ["puae"],
-            "c64": ["vice_x64sc"],
-            "c128": ["vice_x128"],
-            "pet": ["vice_xpet"],
-            "plus4": ["vice_xplus4"],
-            "vic20": ["vice_xvic"],
-            "dos": ["dosbox_pure"],
-            "intv": ["freeintv"]
-        };
+        let rv = CONSTS.cores;
         if (this.isSafari && this.isMobile) {
             rv.n64 = rv.n64.reverse();
         }
         return rv;
     }
     requiresThreads(core) {
-        const requiresThreads = ["ppsspp", "dosbox_pure"];
-        return requiresThreads.includes(core);
+        return CONSTS.requiresThreads.includes(core);
     }
     requiresWebGL2(core) {
-        const requiresWebGL2 = ["ppsspp"];
-        return requiresWebGL2.includes(core);
+        return CONSTS.requiresWebGL2.includes(core);
     }
     getCore(generic) {
         const cores = this.getCores();
@@ -257,7 +218,7 @@ class EmulatorJS {
         return parseInt(rv.join(""));
     }
     constructor(element, config) {
-        this.ejs_version = "4.3.0-beta";
+        this.ejs_version = CONSTS.version;
         this.extensions = [];
         this.allSettings = {};
         this.initControlVars();
